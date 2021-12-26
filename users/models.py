db541a3559baa2cea17b5a3338ad7d7bbaa51653
hosinoz <hosinozzz@gmail.com>
2021-12-27 00:03:00 +0900

    #3.2 First Model Fields

--- users/models.py
+++ users/models.py
@@ -2,6 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(models.Model):
+class User(AbstractUser):
 
-    pass
+    bio = models.TextField(default="")
