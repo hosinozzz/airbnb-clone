008210df3f65d7eb3684f08db33a0ec3ffe7eeb8
hosinoz <hosinozzz@gmail.com>
2022-01-16 00:47:04 +0900

    #11.8 Class Based Views part Two

--- rooms/views.py
+++ rooms/views.py
@@ -3,10 +3,10 @@ from . import models
 
 
 class HomeView(ListView):
-
     """HomeView Definition"""
 
     model = models.Room
     paginate_by = 10
     paginate_orphans = 5
     ordering = "created"
+    context_object_name = "rooms"
