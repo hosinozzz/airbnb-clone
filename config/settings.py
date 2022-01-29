cce65297327bea7392e951192a9a8b7cad28fcd0
hosinoz <hosinozzz@gmail.com>
2022-01-30 02:38:13 +0900

    #16.4 Completing Verification (07:13)

--- config/settings.py
+++ config/settings.py
@@ -146,3 +146,4 @@ EMAIL_HOST = "smtp.mailgun.org"
 EMAIL_PORT = "587"
 EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
 EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "sexy-guy@sandboxe395669115744849b9fca7ece2f71c1d.mailgun.org"
