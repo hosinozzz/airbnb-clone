dcee38cd2b010f1e278f339ff360a4af7fb79927
hosinoz <hosinozzz@gmail.com>
2022-01-30 00:41:46 +0900

    #15.0 Signup Form (09:27)

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 app_name = "users"
-
 urlpatterns = [
     path("login", views.LoginView.as_view(), name="login"),
     path("logout", views.log_out, name="logout"),
+    path("sigup", views.SignUpView.as_view(), name="signup"),
 ]
