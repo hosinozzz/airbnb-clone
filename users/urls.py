baddf4ec5e2bdef10a7399e4775064329bf2fd9c
hosinoz <hosinozzz@gmail.com>
2022-02-01 14:15:38 +0900

    #21.7 Change Password

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 app_name = "users"
-
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("login/github/", views.github_login, name="github-login"),
@@ -15,5 +14,6 @@ urlpatterns = [
         "verify/<str:key>/", views.complete_verification, name="complete-verification"
     ),
     path("update-profile/", views.UpdateProfileView.as_view(), name="update"),
+    path("update-passwod/", views.UpdatePasswordView.as_view(), name="password"),
     path("<int:pk>/", views.UserProfileView.as_view(), name="profile"),
 ]
