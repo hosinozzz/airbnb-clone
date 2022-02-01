4f5f5a8f537a608ae6e8bab9b398a7151179cde3
hosinoz <hosinozzz@gmail.com>
2022-02-01 14:35:43 +0900

    #22.0 Photo Grid

--- users/views.py
+++ users/views.py
@@ -239,28 +239,12 @@ class UpdateProfileView(mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateView
         return form
 
 
-class UpdatePasswordView(SuccessMessageMixin, PasswordChangeView):
-
-    template_name = "users/update-password.html"
-    success_message = "Password Updated"
-
-    def get_form(self, form_class=None):
-        form = super().get_form(form_class=form_class)
-        form.fields["old_password"].widget.attrs = {"placeholder": "Current password"}
-        form.fields["new_password1"].widget.attrs = {"placeholder": "New password"}
-        form.fields["new_password2"].widget.attrs = {
-            "placeholder": "Confirm new password"
-        }
-        return form
-
-
 class UpdatePasswordView(
-    mixins.EmailLoginOnlyView,
     mixins.LoggedInOnlyView,
+    mixins.EmailLoginOnlyView,
     SuccessMessageMixin,
     PasswordChangeView,
 ):
-
     template_name = "users/update-password.html"
     success_message = "Password Updated"
 
