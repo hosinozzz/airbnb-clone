0a64cce2b710409b4e3ac85548b921db3e6ad1f0
hosinoz <hosinozzz@gmail.com>
2022-02-09 01:14:24 +0900

    test

--- config/settings.py
+++ config/settings.py
@@ -188,8 +188,9 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = "airbnb-clone-hosinoz"
-    AWS_AUTO_CREATE_BUCKET = True
-    AWS_BUCKET_ACL = "public-read"
+    # AWS_AUTO_CREATE_BUCKET = True
+    AWS_DEFAULT_ACL = "public-read"
+    # AWS_BUCKET_ACL = "public-read"
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
