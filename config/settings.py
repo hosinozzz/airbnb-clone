9bd418c60dca7745588e17301261f60f5dc13421
hosinoz <hosinozzz@gmail.com>
2022-02-09 02:59:18 +0900

    TEST

--- config/settings.py
+++ config/settings.py
@@ -187,7 +187,7 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = "airbnb-clone-hosinoz"
-    AWS_BUCKET_ACL = "public-read"
+    AWS_DEFAULT_ACL = "public-read"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-1.amazonaws.com"
